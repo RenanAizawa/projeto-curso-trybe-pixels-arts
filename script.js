27da1d1212f986336ad36ce5fb54a745cf05cfea
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2021-12-09 16:07:52 -0500

    Q. 1- 6, resolvidas. Retida dos botoes

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 // Containers de variaveis
 console.log('Ola, mundo! Estou funcionado!!!')
 const pixelBoard = document.getElementById('pixel-board');
-const blackB = document.getElementsByClassName('black');
-const redB = document.getElementsByClassName('red');
-const blueB = document.getElementsByClassName('blue');
-const greenB = document.getElementsByClassName('green');
+
 // Funções Aplicadas
 function addPixels (){
-    for (let i = 0; i < 5; i++){
+    for (let i = 0; i < 5; i++) {
         for (let i = 0; i < 5; i++){
             let divPixel = document.createElement('div');
             let attClass = document.createAttribute('class');
@@ -18,7 +15,3 @@ function addPixels (){
     }
 }
 addPixels()
-function colorSeletec (event){
-    const classSeletec = document.querySelector('.selected');
-    classSeletec.
-}
