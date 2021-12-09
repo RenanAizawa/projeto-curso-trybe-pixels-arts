aebaeb1bbfb7e5279e5c6420830d496e69c814eb
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2021-12-09 15:33:31 -0500

    Q. 1 -6, resolvidas

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // Containers de variaveis
-window.onload
 console.log('Ola, mundo! Estou funcionado!!!')
 const pixelBoard = document.getElementById('pixel-board');
+const blackB = document.getElementsByClassName('black');
+const redB = document.getElementsByClassName('red');
+const blueB = document.getElementsByClassName('blue');
+const greenB = document.getElementsByClassName('green');
 
 // Funções Aplicadas
+addPixels()
 function addPixels () {
-    pixelBoard
+    for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 5; i++) {
+            let divPixel = document.createElement('div');
+            let attClass = document.createAttribute('class');
+            attClass.value = 'pixel';
+            divPixel.setAttributeNode(attClass);
+            pixelBoard.appendChild(divPixel)
+        }    
+    }
 }
