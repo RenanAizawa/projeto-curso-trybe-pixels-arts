e89f48251d348ea6d253e42f877e22dd58126a3e
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2021-12-09 14:10:47 -0500

    Q. 1 - 3, resolvidas

---
+++ script.js
@@ -0,0 +1,9 @@
+// Containers de variaveis
+window.onload
+console.log('Ola, mundo! Estou funcionado!!!')
+const pixelBoard = document.getElementById('pixel-board');
+
+// Funções Aplicadas
+function addPixels () {
+    pixelBoard
+}
