528fa04e37039be65356e124a000ad339d28ebbc
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2021-12-13 17:00:17 -0500

    solucionado o event. target, agora falta o resto

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // Containers de variaveis
 console.log('Ola, mundo! Estou funcionado!!!')
 const pixelBoard = document.getElementById('pixel-board');
-
-// Funções Aplicadas
+var mainColor = document.getElementsByClassName('black');
+// Função de criação das div e class
 function addPixels (){
     for (let i = 0; i < 5; i++) {
+        let creatL = document.createElement('div');
+        let attCl = document.createAttribute('class');
+        attCl.value = "linha " + (i + 1);
+        creatL.setAttributeNode(attCl);
+        pixelBoard.appendChild(creatL)
         for (let i = 0; i < 5; i++){
             let divPixel = document.createElement('div');
             let attClass = document.createAttribute('class');
@@ -14,8 +19,13 @@ function addPixels (){
         }    
     }
 }
-addPixels()
+// Seletetor de cor
 function CorSelected (event) {
     let classSelect = document.querySelector('.selected');
-    
+    classSelect.classList.remove('selected');
+    event.target.className = 'selected'
 }
+
+// Aplicação de funções
+addPixels()
+mainColor.addEventListener('click', CorSelected)
